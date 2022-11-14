f49840b3c54110929f76e5f953d42c6c0a066eb0
Abdullah2213565 <dulakhan024@gmail.com>
2022-11-14 11:04:06 +0100

    Add expenses, models and controllers

---
+++ db/migrate/20221011081520_create_expenses.rb
@@ -0,0 +1,10 @@
+class CreateExpenses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :expenses do |t|
+      t.string :name
+      t.string :amount     
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20221011105937_create_expense_categories.rb
@@ -0,0 +1,8 @@
+class CreateExpenseCategories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :expense_categories do |t|
+
+      t.timestamps
+    end
+  end
+end
