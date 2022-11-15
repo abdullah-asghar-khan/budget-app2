9fcc7c5a16c3d0a9c82b2459881622f8c8ca289b
Abdullah2213565 <dulakhan024@gmail.com>
2022-11-15 16:37:42 +0100

    Project working, only icon left

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Project
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_support.key_generator_hash_digest_class = OpenSSL::Digest::SHA1
   end
 end
